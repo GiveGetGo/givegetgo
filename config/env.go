1c751d4141d079394019016613e166625633f24f
Sean Chang <seanochang@Seans-MacBook-Pro.local>
2024-03-05 22:32:24 -0500

    fix(*): load env take file path

--- config/env.go
+++ config/env.go
@@ -6,9 +6,15 @@ import (
 	"github.com/joho/godotenv"
 )
 
-func LoadEnv() {
+func LoadEnv(filepaths ...string) {
+	fp := ".env"
+
+	if len(filepaths) > 0 {
+		fp = filepaths[0]
+	}
+
 	// Load environment variables
-	err := godotenv.Load()
+	err := godotenv.Load(fp)
 	if err != nil {
 		log.Fatalf("Error loading .env file: %v", err)
 	}
