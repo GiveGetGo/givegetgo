2e6eea43525e878a31c9478b604ed7ff3ca94387
Andy Yang <andyyang@AndydeMacBook-Air.local>
2024-04-05 22:31:27 -0400

    feat: Update user register request

--- types/user.go
+++ types/user.go
@@ -4,8 +4,8 @@ package types
 type RegisterRequest struct {
 	Email    string `json:"email" binding:"required"`
 	Password string `json:"password" binding:"required"`
-	Class string `json:"class" binding:"required"`
-	Major string `json:"major" binding:"required"`
+	Class    string `json:"class" binding:"required"`
+	Major    string `json:"major" binding:"required"`
 }
 
 // LoginRequest - request body for the login endpoint
