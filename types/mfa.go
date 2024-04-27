8b270b9e43c2e74e70816d0c2f41ae941054a263
SeanoChang <chang755@purdue.edu>
2024-04-27 16:36:30 -0400

    feat(type): remove userid from mfa request

--- types/mfa.go
+++ types/mfa.go
@@ -2,12 +2,10 @@ package types
 
 // MFAVerificationRequest
 type MFAVerificationRequest struct {
-	UserID uint   `json:"userID" binding:"required"`
-	Email  string `json:"email" binding:"required"`
+	Email string `json:"email" binding:"required"`
 }
 
 // VerifyMFARequest is the request for verifying MFA
 type VerifyMFARequest struct {
-	UserID           uint   `json:"userID" binding:"required"`
 	VerificationCode string `json:"verification_code" binding:"required"`
 }
